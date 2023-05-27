1527ae65faff37a61926e96b84d3d00a03f6ccb8
Cysiek96 <113532109+Cysiek96@users.noreply.github.com>
2023-05-27 14:30:16 +0200

    Add files via upload

---
+++ f-103-exam/app.js
@@ -0,0 +1,98 @@
+const squares = document.querySelectorAll(".gameBoard div");
+const btn = document.querySelector("button.conf");
+const alertWindow = document.querySelector("div.alertWindow");
+const winningFormat = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let text = "O";
+let iter = 0;
+let isWin = false;
+
+btn.addEventListener("click", () => {
+  if (isWin) {
+    clearTheGame();
+  }
+  alertWindow.classList.remove("show");
+});
+
+squares.forEach((square) => {
+  square.addEventListener("click", () => {
+    if (square.innerText === "") {
+      square.innerText = `${text}`;
+      square.classList.add("circle");
+      checkWin("circle");
+      if (!isWin && iter < 4) {
+        addCross();
+        checkWin("cross");
+      }
+    } else {
+      getAlert();
+    }
+  });
+});
+function clearTheGame() {
+  squares.forEach((square) => {
+    square.innerText = "";
+    square.classList.remove("circle");
+    square.classList.remove("cross");
+    isWin = false;
+    iter = 0;
+  });
+}
+function addCross() {
+  let idx = Math.floor(Math.random() * squares.length);
+  while (squares[idx].innerText !== "") {
+    idx = Math.floor(Math.random() * squares.length);
+  }
+  squares[idx].innerText = "X";
+  squares[idx].classList.add("cross");
+  iter++;
+}
+function getAlert(text = "Do not cheat!", btnText = "That was mistake") {
+  alertWindow.childNodes[1].innerHTML = text;
+  btn.innerText = btnText;
+  alertWindow.classList.add("show");
+}
+function checkWin(classToCheck) {
+  let winCondition = 15;
+  let draw = 9;
+  squares.forEach((square) => {
+    if (square.innerText !== "") {
+      draw--;
+    }
+  });
+  winningFormat.forEach((win) => {
+    win.forEach((idx) => {
+      if (squares[idx].classList.contains(classToCheck)) {
+        winCondition -= +squares[idx].getAttribute("data-value");
+      }
+    });
+
+    if (winCondition == 0) {
+      if (classToCheck === "circle") {
+        getAlert(`Circle wins !!!! Great Job`, "Once again ?");
+        return (isWin = true);
+      } else {
+        getAlert(
+          "Cross wins :(. Will You try to beat this player? ",
+          "Let try to beat him!"
+        );
+        return (isWin = true);
+      }
+    } else {
+      winCondition = 15;
+    }
+  });
+  if (draw === 0) {
+    getAlert("It is draw. Let try to beat the computer", "Once again?");
+    return (isWin = true);
+  }
+}
